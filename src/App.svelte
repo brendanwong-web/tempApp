86051f913862a70becfcee2308aff67cf69dd15e
brendanwong <brendanwong24@gmail.com>
2021-01-23 15:09:22 +0800

    fixed mobile

--- src/App.svelte
+++ src/App.svelte
@@ -73,9 +73,11 @@
 		box-sizing: border-box;
 		margin: 0;
 		width: 100vw;
-		height: 100vh;
+		height: auto;
 		padding-top: 1rem;
+		padding-bottom: 1rem;
 		padding-left: 10px;
+		padding-right: 10px;
 		background: rgba(255,255,255,0.4);
 		border: 1px solid rgba(255,255,255,0.2);
 		backdrop-filter: blur(5px);
